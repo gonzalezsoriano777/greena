05c16f19aaab07182918dc9cf80747236fc68fb9
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-10-17 15:59:29 +0000

    EasyGreen

---
+++ Green.js
@@ -0,0 +1,15 @@
+var potatoe = {
+    name: 'p',
+    color: 'brown',
+    size: 'large',
+    shape: 'oval',
+    
+    
+function(potatoe){
+     this.potatoe.name +  ' & ' + this.potatoe.color;
+ },
+ 
+ function(potatoe){
+     this.potatoe.size + ' & ' + this.potatoe.shape;
+ }
+}
